59e3b9d6a47cee78818802b86941b29564198793
muhammad ebeid <muhammed.ebeid@gmail.com>
2019-12-02 20:32:14 +0200

    basic styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        'varela': ['Varela Round', 'sans-serif'],
+      },
+    }
   },
   variants: {},
   plugins: []
