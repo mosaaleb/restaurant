30f0b13bd57a8a146e28643cf35ad4fe8cd1363b
muhammad ebeid <muhammed.ebeid@gmail.com>
2019-12-02 20:33:02 +0200

    add tab switching logic

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,37 @@
 import '../css/main.css';
+import HomeTab from './tabs/home';
+import MenuTab from './tabs/menu';
+import ContactTab from './tabs/contact';
 
-const text = document.getElementById('test');
-text.innerHTML = `
-<div>Hello From Webpack!</div>
-`;
+const tabs = document.querySelectorAll('.tab');
+tabs[0].classList.add('active-tab');
+HomeTab.render();
+
+
+const renderTab = (tab) => {
+  switch (tab.id) {
+    case 'home':
+      HomeTab.render();
+      break;
+    case 'menu':
+      MenuTab.render();
+      break;
+    default:
+      ContactTab.render();
+      break;
+  }
+};
+
+const resetActiveTab = () => {
+  tabs.forEach((tab) => {
+    tab.classList.remove('active-tab');
+  });
+};
+
+tabs.forEach((tab) => {
+  tab.addEventListener('click', () => {
+    resetActiveTab();
+    tab.classList.add('active-tab');
+    renderTab(tab);
+  });
+});
