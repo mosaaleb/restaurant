ceed5226115773594c682dbbc27120c6bdf59413
muhammad ebeid <muhammed.ebeid@gmail.com>
2019-12-03 23:43:49 +0200

    custom scrollbar styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,16 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
+        'vibes': ['Great Vibes', 'cursive'],
         'varela': ['Varela Round', 'sans-serif'],
       },
+      spacing: {
+        '80': '18rem',
+        '88': '20rem',
+        '96': '22rem',
+        '104':'24rem',
+        '112': '26rem',
+      }
     }
   },
   variants: {},
